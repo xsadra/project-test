d3b025a4074d6723a2e187ebdf85427746d2a670
Sadra <ti9mail@gmail.com>
2018-05-04 21:36:52 +0200

    add structure

---
+++ moodle/src/main/java/de/sadrab/moodle/MoodleApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.moodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodleApplication.class, args);
+	}
+}
